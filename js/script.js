64723cc79933bd83ec7b9e250dbbf90331e55540
Brayan <bittolo.8bit@gmail.com>
2026-01-14 17:51:44 +0100

    Add TODO list

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+# ToDo
+- 5 numeri casuali
+- timer di 30 secondi
+- i numeri scompaiono
+- 5 input per l'utente
+    - validazione: no caratteri diversi da numeri, no numeri uguali
+    - segnala in caso di input non valido
+- verifica dei numeri
+- responso
+*/
